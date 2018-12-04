b54cc0d16fe125394488c9febcbd08fa771b1d23
rb <rbiojout@gmail.com>
2018-12-04 13:17:36 +0100

    downgrade celery to 4.1.1

--- ductilis/exchange/tasks.py
+++ ductilis/exchange/tasks.py
@@ -1,10 +1,19 @@
+# import the logging library
+import logging
+import timeit
+
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 
 from celery.decorators import task
 from celery.utils.log import get_task_logger
+from ductilis.exchange.models import Ticker
+
 
 from ductilis.exchange.providers import iex_call, yahoo_call, quandl_call
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 #@periodic_task(run_every=(crontab(minute='*/15')), name="call_iex", ignore_result=True)
 @task(name="call_iex")
@@ -27,4 +36,13 @@ def call_quandl_task():
 
 @task(name="call_yahoo")
 def call_yahoo_task():
-    return yahoo_call.create_tickers()
+    tickers = Ticker.objects.all()
+    for item in tickers:
+        start = timeit.default_timer()
+        ticker = item.symbol
+        logger.debug("*********** start with ticker ", ticker)
+
+        yahoo_call.fill_ticks(ticker)
+        end = timeit.default_timer()
+        logger.debug("persist ticker ", ticker, ' Time: ', end - start)
+    return
